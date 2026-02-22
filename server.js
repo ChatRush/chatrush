8a4a1b824c68aba290377cd822239e9414a0570f
Varun kumar <svarunkumar6629@gmail.com>
2026-02-22 12:53:50 +0530

    Fix home route

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 // ================= TEMP USERS (resets on restart) =================
 // NOTE: For real production, use DB (MongoDB/SQLite). For now OK.
